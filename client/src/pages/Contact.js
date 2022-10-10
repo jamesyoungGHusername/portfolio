2d2e9db24cd32ba0b4368ad744b9deede7cb24a9
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-10-10 00:02:26 -0600

    form is finished

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 function Contact(){
     const [contact,setContact] = useState({
-        email:"",
-        name:"",
-        message:""
+            email:"",
+            name:"",
+            message:""
     });
     const [showMsg,setShowMsg] = useState(false);
     const handleChange = (e) => {
@@ -13,14 +13,31 @@ function Contact(){
     const displayInfoRequired = (e) => {
         if(e.target.value == ""){
             console.log(e.target.name + " is blank");
-            e.target.className = "red";
+            if(e.target.name != "message"){
+                e.target.className = "red";
+            }
+            
         }else{
             e.target.className = "";
         }
     };
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target);
+        if(contact.email && contact.name){
+            e.preventDefault();
+            console.log(e.target);
+            //this is the true, correct email validation
+            if(contact.email.toLowerCase().match(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/
+            )){
+                console.log("valid email");
+                setContact({email:"" ,name:"",message:""})
+            }else{
+                console.log("invalid email");
+                alert('Invalid Email');
+            }
+        }else{
+            alert("Email and name required");
+        }
+        
     };
     return (
         <section id="contact">
