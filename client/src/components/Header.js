6b9e90d9d6165090a59741a51960ef8a1d19b4ad
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-11-22 15:01:59 -0700

    changed header component to display home tab styling correctly on initial load

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -62,7 +62,7 @@ export default function Header(props) {
         <h4 className='displayedHeader'>Guest@JamesMakesApps.com % {displayedHeader}&#9608;</h4>
         <nav>
             <ul>
-                <Link className='navLink' to="/"><li style={(header=="home")?{backgroundColor:"rgb(18, 9, 9)",color:"white"}:null} onClick={()=>updateHeader(" ")}>home</li></Link>
+                <Link className='navLink' to="/"><li style={(header==" ")?{backgroundColor:"rgb(18, 9, 9)",color:"white"}:null} onClick={()=>updateHeader(" ")}>home</li></Link>
                 <Link className='navLink' to="/about_me"><li style={(header=="about_me")?{backgroundColor:"rgb(18, 9, 9)",color:"white"}:null} onClick={()=>updateHeader("about_me")}>about_me</li></Link>
                 <Link className='navLink' to="/projects"><li style={(header=="projects")?{backgroundColor:"rgb(18, 9, 9)",color:"white"}:null} onClick={()=>updateHeader("projects")}>projects</li></Link>
                 <Link className='navLink' to="/contact"><li style={(header=="contact")?{backgroundColor:"rgb(18, 9, 9)",color:"white"}:null} onClick={()=>updateHeader("contact")}>contact</li></Link>
